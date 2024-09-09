23d3f5272655f5daeff14850c9be300032667a3c
JesusGPriego <suleware@gmail.com>
2024-09-09 13:39:44 +0200

    updated .gitignore

--- dist/src/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("module-alias/register");
-const sampleFunction_1 = require("@/utils/sampleFunction");
-console.log('sampleFunction', (0, sampleFunction_1.sampleFunction)());
